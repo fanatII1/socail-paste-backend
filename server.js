d52b1c3dcf60c4d421b25a56bcf4412e60dc5f1c
Mandla-CR7 <mandla.nkosi@healsgood.com>
2025-05-08 18:06:27 +0200

    new change

--- server.js
+++ server.js
@@ -16,28 +16,28 @@ import dns from 'dns';
 dotenv.config();
 const app = express();
 const PORT = 5000;
-// const serviceAccount = {
-//   type: process.env.FIREBASE_TYPE,
-//   project_id: process.env.FIREBASE_PROJECT_ID,
-//   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-//   private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-//   client_email: process.env.FIREBASE_CLIENT_EMAIL,
-//   client_id: process.env.FIREBASE_CLIENT_ID,
-//   auth_uri: process.env.FIREBASE_AUTH_URI,
-//   token_uri: process.env.FIREBASE_TOKEN_URI,
-//   auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-//   client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-// };
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+};
 
-// console.log(serviceAccount)
+console.log(serviceAccount)
 
-// initializeApp({
-//   credential: cert(serviceAccount),
-//   storageBucket: 'gs://replica-paste.appspot.com',
-// });
+initializeApp({
+  credential: cert(serviceAccount),
+  storageBucket: 'gs://replica-paste.appspot.com',
+});
 
-// const bucket = getStorage().bucket();
-// const db = getFirestore();
+const bucket = getStorage().bucket();
+const db = getFirestore();
 
 
 
